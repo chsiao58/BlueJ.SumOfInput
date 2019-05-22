c5f1173289022724df5b056619ceb57a32c0f6b6
Joanna Hsiao <chsiao58@gmail.com>
2019-05-22 11:58:00 -0400

    add the functionality into the program

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 /**
- * Created by iyasuwatts on 10/17/17.
+ * This program take ant interger input n from user,
+ * and then it will print the sum of the numbers from 1 to n
+ * 
+ * Create by Joanna Hsiao
  */
 public class Main {
 
-    public static void main(String[] args){
-
+    public static void main()
+    {
+        int userNum = 0;
+        int sum = 0;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number.");
+        
+        userNum = input.nextInt();
+        
+        for(int i = 1; i <= userNum; i++)
+            sum += i;
+            
+        System.out.println("The sum is from 1 to " 
+        + userNum + " is "+ sum);
     }
 }
